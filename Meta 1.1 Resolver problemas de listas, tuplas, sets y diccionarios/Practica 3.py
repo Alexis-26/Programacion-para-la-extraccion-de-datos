53ab6ec14503bdf7d709a50c81cd5dad3580d494
Alexis-26 <alexis.miros@uabc.edu.mx>
2024-02-16 22:31:37 -0800

    Modificacion de codigo

---
+++ Meta 1.1 Resolver problemas de listas, tuplas, sets y diccionarios/Practica 3.py
@@ -0,0 +1,20 @@
+"""Alexis Cruz Miros 951 13/02/2024
+
+"""
+
+
+#Lista de tuplas con ciudad, poblacion y área
+ciudades = [
+("Tijuana", 5, "NoroEste"), 
+("Ciudad de Mexico", 8, "Centro"), 
+("Ensenada", 3, "NoroEste"), 
+("Puebla", 3, "Centro"), 
+("Cancun", 4, "Sur")
+]
+
+#Ciclo for para hacer las funciones, ademas tomar en cuenta que se debe de guardar en un diccionario la ciudad y el resultado de la densidad
+for index in ciudades:
+    datos_recibidos = index
+    print(datos_recibidos)
+
+#print(ciudades)
